323e9ae96c0e49914fe870d19025b451ceda749c
Harald Glatt <code@hachre.de>
2015-05-01 20:21:15 +0200

    0.1.20150501.1
    initial commit

---
+++ source/percent.ts
@@ -0,0 +1,13 @@
+declare var Percent:any;
+
+class p {
+	constructor() {
+		console.log("Percent launching...");
+	}
+
+    public Run(): void {
+		console.log("Run pressed");
+    }
+}
+
+Percent = new p();
