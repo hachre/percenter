70024e73944375927fb1948dc63f055138c045d4
Harald Glatt <code@hachre.de>
2015-05-03 22:24:53 +0200

    0.8.20150503.3
    removed: button
    added: execution on value change
    changed: minor style updates

--- source/UI.ts
+++ source/UI.ts
@@ -1,47 +1,59 @@
 declare var Percenter: any;
 
 class UI {
-	private hdlInputPercent1: HTMLInputElement;
-	private hdlInputPercent2: HTMLInputElement;
-	private hdlInputResultAugmentation: HTMLInputElement;
+	private hdlInputValue1: HTMLInputElement;
+	private hdlInputValue2: HTMLInputElement;
+	private hdlInputMultiplier: HTMLInputElement;
 	private hdlButtonRun: HTMLButtonElement;
 	private hdlOutput: HTMLDivElement;
 
 	constructor() {
-		this.hdlInputPercent1 = <HTMLInputElement>document.getElementById("InputPercent1");
-		this.hdlInputPercent2 = <HTMLInputElement>document.getElementById("InputPercent2");
-		this.hdlInputResultAugmentation = <HTMLInputElement>document.getElementById("InputResultAugmentation");
+		this.hdlInputValue1 = <HTMLInputElement>document.getElementById("InputValue1");
+		this.hdlInputValue2 = <HTMLInputElement>document.getElementById("InputValue2");
+		this.hdlInputMultiplier = <HTMLInputElement>document.getElementById("InputMultiplier");
 		this.hdlButtonRun = <HTMLButtonElement>document.getElementById("ButtonRun");
 		this.hdlOutput = <HTMLDivElement>document.getElementById("Output");
 
-		this.hdlButtonRun.addEventListener("click", () => {
-			// Read the current inputs from the UI and calculate the percentage.
-			Percenter.Calc.Value1 = this.getValue1();
-			Percenter.Calc.Value2 = this.getValue2();
-			Percenter.Calc.ResultAugmentation = this.getResultAugmentation() || 1;
-			var percentage = Percenter.Calc.getPercentage();
-			
-			// Set the result in the UI.
-			this.setOutput(percentage);
-		});
+		// Bind the events that trigger a recalculation		
+		this.hdlButtonRun.addEventListener("click", () => { this.executeCalculation(); });
+		this.hdlInputValue1.addEventListener("keypress", () => { this.executeCalculation(); });
+		this.hdlInputValue1.addEventListener("keyup", () => { this.executeCalculation(); });
+		this.hdlInputValue1.addEventListener("change", () => { this.executeCalculation(); });
+		this.hdlInputValue2.addEventListener("keypress", () => { this.executeCalculation(); });
+		this.hdlInputValue2.addEventListener("keyup", () => { this.executeCalculation(); });
+		this.hdlInputValue2.addEventListener("change", () => { this.executeCalculation(); });
+		this.hdlInputMultiplier.addEventListener("keypress", () => { this.executeCalculation(); });
+		this.hdlInputMultiplier.addEventListener("keyup", () => { this.executeCalculation(); });
+		this.hdlInputMultiplier.addEventListener("change", () => { this.executeCalculation(); });
 
 		console.log("UI ready...");
 	}
 
+	private executeCalculation(): void {
+		// Read the current inputs from the UI and calculate the percentage.
+		Percenter.Calc.Value1 = this.getValue1() || "nope";
+		Percenter.Calc.Value2 = this.getValue2() || "nope";
+		Percenter.Calc.ResultAugmentation = this.getResultAugmentation() || 1;
+		var percentage = Percenter.Calc.getPercentage();
+			
+		// Set the result in the UI.
+		this.setOutput(percentage);
+	}
+
 	private getValue1(): number {
-		return Number(this.cleanDecimal(this.hdlInputPercent1.value));
+		return Number(this.cleanDecimal(this.hdlInputValue1.value));
 	}
 
 	private getValue2(): number {
-		return Number(this.cleanDecimal(this.hdlInputPercent2.value));
+		return Number(this.cleanDecimal(this.hdlInputValue2.value));
 	}
-	
+
 	private getResultAugmentation(): number {
-		return Number(this.hdlInputResultAugmentation.value);
+		return Number(this.hdlInputMultiplier.value);
 	}
 
 	private setOutput(value: number): void {
-		this.hdlOutput.textContent = value.toFixed(2).replace(".00","") + "%";
+		this.hdlOutput.textContent = (value.toFixed(2).replace(".00", "") + "%").replace("NaN%", "").replace("Infinity%", "");
 	}
 
 	private cleanDecimal(value: string): string {
